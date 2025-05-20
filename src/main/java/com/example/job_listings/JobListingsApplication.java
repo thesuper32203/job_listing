ec50c8c41d177c731c3072241b55e487ea371997
Anthony <thesuper32203@gmail.com>
2025-05-19 22:46:23 -0400

    first commit

---
+++ src/main/java/com/example/job_listings/JobListingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_listings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobListingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/job_listings/JobListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_listings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
